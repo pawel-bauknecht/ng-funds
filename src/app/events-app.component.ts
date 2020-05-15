4c6dc129b98824e385a2befe13502a3748d1b3d3
Pawel Bauknecht <pawelbauknecht@gmail.com>
2020-05-15 14:48:03 +0200

    2. getting started - end

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  templateUrl: './events-app.component.html',
+  styleUrls: ['./events-app.component.scss'],
+})
+export class EventsAppComponent {}
