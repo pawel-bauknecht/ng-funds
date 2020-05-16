42c6eb54152d5ebedf43c2fed1a2665767d403da
Pawel Bauknecht <pawelbauknecht@gmail.com>
2020-05-16 10:07:55 +0200

    3. communicating between angular components + navbar - end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavbarComponent } from './nav/navbar.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [EventsAppComponent],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavbarComponent,
+  ],
   providers: [],
   bootstrap: [EventsAppComponent],
 })
---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.scss'],
+})
+export class EventThumbnailComponent implements OnInit {
+  @Input() event: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
